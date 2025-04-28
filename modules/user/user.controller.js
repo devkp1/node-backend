e1501be9eb5284a4236c0a2969d372ca419dab6f
Devanshu <devanshukp401@gmail.com>
2025-04-28 17:27:23 +0530

    remove: unncessary imports

--- modules/user/user.controller.js
+++ modules/user/user.controller.js
@@ -1,6 +1,4 @@
 import userModel from './user.model.js';
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcryptjs';
 import { successResponse, errorResponse } from '../../utils/responseHandler.js';
 import {
   getHashPassword,
