8486431631579b3bb916d9d093fd351d2a04adc8
Devanshu <devanshukp401@gmail.com>
2025-05-06 14:43:35 +0530

    add: updateUserGenderDob api

--- modules/user/user.route.js
+++ modules/user/user.route.js
@@ -1,9 +1,17 @@
 import express from 'express';
-import { loginUser, registerUser } from './user.controller.js';
+import {
+  loginUser,
+  registerUser,
+  updateUserGenderDob,
+} from './user.controller.js';
 import { isAuthenticateUser } from '../../middleware/validateTokenHandler.js';
+import { verifyToken } from '../../middleware/verifyTokenHandler.js';
 const userRoute = express.Router();
 
 userRoute.route('/register-user').post(registerUser);
 userRoute.route('/signin').post(isAuthenticateUser, loginUser);
+userRoute
+  .route('/update-gender-dob')
+  .put(isAuthenticateUser, verifyToken, updateUserGenderDob);
 
 export default userRoute;
